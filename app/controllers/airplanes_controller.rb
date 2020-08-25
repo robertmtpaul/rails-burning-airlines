2e48c9da1993bb146e608f602957db3d29d66415
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-08-25 22:40:06 +1000

    Crud for flights and airplanes

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -1,22 +1,42 @@
 class AirplanesController < ApplicationController
   def new
+    @airplane = Airplane.new
   end
 
   def create
+    @airplane = Airplane.create airplane_params
+    redirect_to airplanes_path
   end
 
   def index
+    @airplanes = Airplane.all
   end
 
   def show
+    @airplane = Airplane.find params[:id]
+    @flights = @airplane.flights
   end
 
   def edit
+    @airplane = Airplane.find params[:id]
   end
 
   def update
+    airplane = Airplane.find params[:id]
+    airplane.update airplane_params
+    redirect_to airplanes_path
   end
 
   def destroy
+    airplane = Airplane.find params[:id]
+    airplane.destroy
+    redirect_to airplanes_path
+  end
+
+
+  private
+
+  def airplane_params
+    params.require(:airplane).permit(:name, :rows, :columns)
   end
 end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,22 +1,48 @@
 class FlightsController < ApplicationController
   def new
+    @flight = Flight.new
+    @airports = Airport.all.map{|a| [a.name, a.code]}
+    @airplanes = Airplane.all.map{|a| [a.name, a.id]}
   end
 
   def create
+    @flight = Flight.create flight_params
+    redirect_to flights_path
   end
 
   def index
+    @flights = Flight.all
   end
 
   def show
+    @flight = Flight.find params[:id]
+    @airplane = Airplane.find @flight.airplane_id
+    @origin = Airport.find_by(code: @flight.origin)
+    @destination = Airport.find_by(code: @flight.destination)
   end
 
   def edit
+    @flight = Flight.find params[:id]
+    @airports = Airport.all.map{|a| [a.name, a.code]}
+    @airplanes = Airplane.all.map{|a| [a.name, a.id]}
   end
 
   def update
+    flight = Flight.find params[:id]
+    flight.update flight_params
+    redirect_to flights_path
   end
 
   def destroy
+    flight = Flight.find params[:id]
+    flight.destroy
+    redirect_to flights_path
+  end
+
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:flight_number, :origin, :destination, :date, :airplane_id)
   end
 end
