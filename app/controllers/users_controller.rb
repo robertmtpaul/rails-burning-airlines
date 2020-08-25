309397270ae2389cdedcda1da69828967d0f75b8
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-08-25 20:46:49 +1000

    CRUD for Users finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create user_params
+    redirect_to users_path
   end
 
   def index
-  end
-
-  def show
+    @users = User.all
   end
 
   def edit
+    @user = User.find params[:id]
   end
 
   def update
+    user = User.find params[:id]
+    user.update user_params
+    redirect_to users_path
   end
 
   def destroy
+    user = User.find params[:id]
+    user.destroy
+    redirect_to users_path
+    
+  end
+
+  #private methods below
+  private
+
+  def user_params
+    #Submitted params MUST be the below
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
