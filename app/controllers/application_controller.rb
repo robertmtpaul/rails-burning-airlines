16c162579a93d90e720690cba7e3bb0215bed5d2
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-08-27 14:51:49 +1000

    Added security measures so only admins can login to the backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
     if session[:user_id].present?
       #.find_by instead of .find so if it's not present it won't be an error.
       @current_user = User.find_by id: session[:user_id]
+      if @current_user.admin == false
+        flash[:error] = "ERROR: Only Admins are able to log into this system."
+        session[:user_id] = nil
+        redirect_to login_path
+      end
     end
     #force logout if user ID isn't found.
     unless session[:user_id].present?
